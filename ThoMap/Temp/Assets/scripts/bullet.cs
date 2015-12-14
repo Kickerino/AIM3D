dce8cfad5aafabfca602dde259d6b252795a181e
Thomastert <thomas@thoemda.com>
2015-12-14 11:33:39 +0100

    houses + button start

---
+++ ThoMap/Temp/Assets/scripts/bullet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet : MonoBehaviour {
+    public float lifetime = 6.0f;
+    public GameObject explosion;
+
+	void Start ()
+    {
+	
+	}
+
+    void Awake()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    void OnCollisionEnter(Collision coll) 
+    {
+        if(coll.gameObject.tag == "house")
+        {
+            Instantiate(explosion, transform.position, Quaternion.Euler(0, 270, 0));
+            Destroy(this.gameObject);
+        }
+
+    }
+}
---
+++ ThoMap/Temp/Assets/scripts/deleteParticle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class deleteParticle : MonoBehaviour {
+    public float lifetime = 1.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+    void Awake()
+    {
+        Destroy(gameObject, lifetime);
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
